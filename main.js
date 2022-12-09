c9b19159c2b8b5514f8f587cb3d76d231b37a977
Mateus Schulle <89845962+mateusschulle@users.noreply.github.com>
2022-12-09 17:52:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+let botaoIniciar = document.querySelector('.formulario__botao');
+let horasTela = document.querySelector('.horas');
+let minutosTela = document.querySelector('.minutos');
+let segundosTela = document.querySelector('.segundos');
+let formulario = document.querySelector('.formulario');
+
+let inputHoras = 0;
+let inputMinutos = 0;
+let inputSegundos = 0;
+
+botaoIniciar.addEventListener('click', (event) =>{
+    event.preventDefault();
+    inputHoras = document.querySelector('.hora__input').value;
+    inputMinutos = document.querySelector('.minuto__input').value;
+    inputSegundos = document.querySelector('.segundo__input').value;
+    if(inputHoras > 0) {
+        horasTela.innerHTML = inputHoras;
+    }
+    if(inputMinutos > 0 && inputMinutos <= 60) {
+        minutosTela.innerHTML = inputMinutos;
+    }
+    if(inputSegundos > 0 && inputSegundos <= 60) {
+        segundosTela.innerHTML = inputSegundos;
+    }
+
+    setInterval(atualizaTemporizador, 1000);
+    formulario.reset();
+})
+
+function atualizaTemporizador() {   
+    if(inputSegundos > 0) {
+        inputSegundos = inputSegundos - 1;
+        segundosTela.innerHTML = inputSegundos;
+    } else if (inputMinutos > 0) {
+        inputMinutos = inputMinutos - 1;
+        minutosTela.innerHTML = inputMinutos;
+        inputSegundos = 59;
+        segundosTela.innerHTML = inputSegundos;
+    } else if(inputHoras > 0) {
+        inputHoras = inputHoras - 1;
+        horasTela.innerHTML = inputHoras;
+        inputMinutos = 59;
+        minutosTela.innerHTML = inputMinutos;
+        inputSegundos= 59;
+        segundosTela.innerHTML = inputSegundos;
+    }
+}
+
+let zerarTemporizador = document.querySelector('.botao__zerar');
+zerarTemporizador.addEventListener('click', () => {
+   location.reload();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
